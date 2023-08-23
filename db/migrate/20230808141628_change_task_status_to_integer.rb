c7f6ed51e372ea56d2685900c7cf9729bc78770c
Marmblshko <mks316skat@gmail.com>
2023-08-23 19:37:31 +0600

    Fixed missed migration where status changed type from string to integer

--- db/migrate/20230808141628_change_task_status_to_integer.rb
+++ db/migrate/20230808141628_change_task_status_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeTaskStatusToInteger < ActiveRecord::Migration[7.0]
   def change
-    change_column :tasks, :status, :integer, default: 0
+    change_column :tasks, :status, :integer, using: 'status::integer', default: 0
   end
 end
